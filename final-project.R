d9edbc5549a67105a1ff0992c38cae88a2043a0d
Giovani Anhesini Bezerra <gioanhesini@gmail.com>
2019-11-04 22:07:40 -0300

    Initial Commit
    Upload of dataset & descriptions

---
+++ final-project.R
@@ -0,0 +1,17 @@
+# Escolher um dataset de domínio público, que apresente artigos científicos
+# associados. Realizar a leitura de pelo menos um artigo, e discutí-lo, como parte da
+# introdução do problema.
+
+# Realizar o pré-processamento dos dados, eliminando eventuais ruídos e dados
+# faltantes.
+
+# Entender o dataset, e apresentar gráficos descritivos, com análises, que
+# representem os dados. Para isso, será necessário utilizar técnicas de visualização
+# de dados.
+
+# Se for pertinente, aplicar técnicas de redução de dimensionalidade
+
+# Aplicar um dos algoritmos estudados em Inteligência Artificial. Discutir a tarefa de
+# aprendizado de máquinas sendo tratada, e a escolha do algoritmo.
+
+# Apresentar e discutir os resultados obtidos.
