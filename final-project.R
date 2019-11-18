aa3012307128d2ae04675f9fd554e30420017e74
Giovani <gioanhesini@gmail.com>
2019-11-18 12:09:46 -0300

    Updates on ASD studies

--- final-project.R
+++ final-project.R
@@ -6,6 +6,9 @@ library(dslabs)
 library(dplyr)
 library(ggplot2)
 
+# Package for 3D plots
+library(scatterplot3d)
+
 # Creating dataframe object & reading the dataset file
 path <- './Autism-Adult-Data.arff'
 dataframe_autism <- data.frame()
@@ -22,19 +25,25 @@ dataframe_autism <- na.omit(dataframe_autism)
 names(dataframe_autism)[21] <- 'class_asd'
 
 # CASE 01 - Relation between occurrences of jundice in patients diagnosed with ASD
+# Filter cases of ASD positive on occurrences of patients born with jundice
+filter_jundice_asd <- filter(dataframe_autism, jundice %in% c("yes"))
+    
 # Get total of 'yes' & 'no' on class_asd
-count(dataframe_autism, class_asd)
-jundice_with_asd <- 180
-jundice_without_asd <- 428
+count(filter_jundice_asd, class_asd)
+jundice_with_asd <- 28
+jundice_without_asd <- 31
 
 # Set values for slices of pie chart
 slices <- c(jundice_with_asd, jundice_without_asd)
 
-# Set labels for slices of pie chart with rounded for 100%
+# Set labels for slices of pie chart
 lbls <- c('ASD', 'Non-ASD')
+# Rounds values to 100% format
 pct <- round(slices / sum(slices)*100)
-lbls <- paste(lbls, pct) # add percents to labels
-lbls <- paste(lbls,'%', sep = '') # ad % to labels
+# Add percents to labels
+lbls <- paste(lbls, pct)
+# Add % to labels
+lbls <- paste(lbls,'%', sep = '')
 
 # Pie chart plot
 pie(slices,
@@ -43,6 +52,37 @@ pie(slices,
     main = 'Occurrences of Autism on Jundice',
     sub = 'Percentage times that Autism Spectrum Disorder (ASD) happenned on positive cases of Jundice'
     )
+
+# CASE 02 - Ethnicity x ASD positive
+barplot(
+    
+        )
+
+# CASE 03 - Ethnicity x Jundice x ASD posittive
+colors <- c("#999999", "#E69F00", "#56B4E9")
+scatterplot3d(filter_jundice_asd,
+              x = filter_jundice_asd$ethnicity,
+              y = filter_jundice_asd$jundice,
+              z = filter_jundice_asd$class_asd,
+              main = 'Ethnicity x Jundice x ASD posittive',
+              sub = '',
+              xlab = '',
+              ylab = '',
+              zlab = '',
+              color = colors
+              )
+
+# CASE 04 - 
+# CASE 05 - 
+# CASE 06 - 
+# CASE 07 - 
+# CASE 08 - 
+
+
+# analisar % de quando preenchido pela propria pessoa / outros com ASD
+
+
+
 
 
 # Entender o dataset, e apresentar gráficos descritivos, com análises, que
